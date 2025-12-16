26e0ac34251c71ce86f132259185a6f937d3dd7c
Minh Ngoc Nguyen <minh@certideal.com>
2025-12-16 16:47:41 +0100

    open-props.style

---
+++ src/Window.tsx
@@ -0,0 +1,20 @@
+import './Window.css'
+
+function Window() {
+
+  return (
+    <>
+      <div>
+        
+      </div>
+        <div id="left">
+          left
+        </div>
+        <div id="right">
+          window1
+        </div>
+    </>
+  )
+}
+
+export default Window
